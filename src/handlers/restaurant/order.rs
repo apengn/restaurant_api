f772871d4b7c53f99ccb7b8020da076391e1239e
apengn <1399094187@qq.com>
2023-11-15 19:47:09 +0800

    feat: add restaurant order get list http api
    Signed-off-by: apengn <1399094187@qq.com>

--- src/handlers/restaurant/order.rs
+++ src/handlers/restaurant/order.rs
@@ -1,5 +1,12 @@
+use crate::db::Pool;
 use crate::handlers::restaurant::RestaurantAuthSession;
-use axum::{http::StatusCode, response::IntoResponse};
+use crate::handlers::{internal_error, Pagination, PaginationReponse};
+use crate::model::order::Order;
+use crate::schema;
+use axum::extract::{Path, Query, State};
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use diesel::prelude::*;
+use diesel_async::RunQueryDsl;
 
 pub async fn create(auth_session: RestaurantAuthSession) -> impl IntoResponse {
     match auth_session.user {
@@ -9,11 +16,49 @@ pub async fn create(auth_session: RestaurantAuthSession) -> impl IntoResponse {
     }
 }
 
-pub async fn list(auth_session: RestaurantAuthSession) -> impl IntoResponse {
+pub async fn list(
+    auth_session: RestaurantAuthSession,
+    State(pool): State<Pool>,
+    state: Option<Path<i32>>,
+    pagination: Option<Query<Pagination>>,
+) -> Result<Json<PaginationReponse<Vec<Order>>>, (StatusCode, String)> {
     match auth_session.user {
-        Some(_user) => "protected".into_response(),
+        Some(user) => {
+            let mut conn = pool.get().await.map_err(internal_error)?;
 
-        None => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+            let Query(pagination) = pagination.unwrap_or_default();
+
+            use schema::orders::dsl::{created_at, orders, restaurant_id, state as order_state};
+            let orders_v = orders
+                .filter(restaurant_id.eq(user.id))
+                .filter(order_state.eq(state.unwrap().0))
+                .limit(pagination.page_size)
+                .offset(pagination.offset())
+                .order(created_at.desc())
+                .select(orders::all_columns())
+                .load::<Order>(&mut conn)
+                .await
+                .map_err(internal_error)?;
+
+            let c = orders
+                .filter(restaurant_id.eq(user.id))
+                .filter(order_state.eq(state.unwrap().0))
+                .count()
+                .get_result::<i64>(&mut conn)
+                .await
+                .map_err(internal_error)?;
+
+            let pagination_response = PaginationReponse {
+                data: orders_v,
+                current_page: pagination.page,
+                page_size: pagination.page_size,
+                total: c,
+            };
+
+            Ok(Json(pagination_response))
+        }
+
+        None => Err((StatusCode::UNAUTHORIZED, "".to_string())),
     }
 }
 
--- src/handlers/restaurant/order_detail.rs
+++ src/handlers/restaurant/order_detail.rs
@@ -1,5 +1,13 @@
+use crate::db::Pool;
+use crate::handlers::internal_error;
 use crate::handlers::restaurant::RestaurantAuthSession;
-use axum::{http::StatusCode, response::IntoResponse};
+use crate::model::order::{Order, OrderParamsResponse};
+use crate::model::order_detail::OrderDetail;
+use crate::schema;
+use axum::extract::{Query, State};
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use diesel::prelude::*;
+use diesel_async::RunQueryDsl;
 
 pub async fn create(auth_session: RestaurantAuthSession) -> impl IntoResponse {
     match auth_session.user {
@@ -9,18 +17,45 @@ pub async fn create(auth_session: RestaurantAuthSession) -> impl IntoResponse {
     }
 }
 
-pub async fn list(auth_session: RestaurantAuthSession) -> impl IntoResponse {
+pub async fn get(
+    auth_session: RestaurantAuthSession,
+    State(pool): State<Pool>,
+    id: Option<Query<i32>>,
+) -> Result<Json<OrderParamsResponse>, (StatusCode, String)> {
     match auth_session.user {
-        Some(_user) => "protected".into_response(),
+        Some(user) => {
+            let mut conn = pool.get().await.map_err(internal_error)?;
+            use schema::orders::dsl::{id as oid, orders, wx_open_id};
 
-        None => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
-    }
-}
+            let order: Order = orders
+                .filter(wx_open_id.eq(user.id))
+                .filter(oid.eq(id.unwrap().0))
+                .select(orders::all_columns())
+                .get_result::<Order>(&mut conn)
+                .await
+                .map_err(internal_error)?;
 
-pub async fn get(auth_session: RestaurantAuthSession) -> impl IntoResponse {
-    match auth_session.user {
-        Some(_user) => "protected".into_response(),
+            use schema::orders_details::dsl::{order_id, orders_details};
+            let ods = orders_details
+                .filter(order_id.eq(order.id))
+                .select(orders_details::all_columns())
+                .get_results::<OrderDetail>(&mut conn)
+                .await
+                .map_err(internal_error)?;
 
-        None => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+            let data = OrderParamsResponse {
+                uuid: order.uuid,
+                restaurant_id: order.restaurant_id,
+                user_id: order.user_id,
+                wx_open_id: order.wx_open_id,
+                qrcode_location_id: order.qrcode_location_id,
+                state: order.state,
+                total_cost: order.total_cost,
+                details: ods,
+            };
+            Ok(Json(data))
+        }
+
+        None => Err((StatusCode::UNAUTHORIZED, "".to_string())),
     }
 }
